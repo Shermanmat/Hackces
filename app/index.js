51ca21bd2b013a52d38ba5a36451063e6c20447c
Jordan Jones <jordanalanjones@gmail.com>
2016-10-18 23:30:56 -0500

    Setting up node server

---
+++ app/index.js
@@ -0,0 +1,33 @@
+// require('./config/connect.js')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const express = require('express')
+const app = express()
+const port = 80
+
+console.log(__dirname)
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened')
+    }
+
+    console.log('server is listening on ', port)
+})
+
+// HTML
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+// Routes
+
+app.get('/', (request, response) => {
+    response.render('index', {
+        title: 'Home'
+    })
+})
